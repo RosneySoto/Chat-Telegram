e67418fdb19e9dcae050de7edc6d5f0c549930f0
Jose Alvarez <rosney.sotor@gmail.com>
2022-01-27 01:09:18 -0300

    Se comienza a configurar el socket

--- components/message/controller.js
+++ components/message/controller.js
@@ -1,4 +1,5 @@
 const store = require('./store');
+const socket = require('../../socket').socket;
 
 function addMessage(chat, user, message, file){
 
@@ -26,6 +27,9 @@ function addMessage(chat, user, message, file){
         }
         console.log(fullMessage);   
         store.add(fullMessage);
+
+        socket.io.emit('message', fullMessage)
+
         resolve(fullMessage);
     });
     
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
 const bodyParser = require('body-parser');
-
+const socket = require('./socket');
 const db = require('./db');
-
 //const router = require('./components/message/network');
 const router = require ('./network/routes');
 
 db('mongodb+srv://admin:123456789*@curso-platzi.t5k97.mongodb.net/test');
 
-var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+socket.connect(server);
 
 router(app);
 
@@ -19,5 +23,6 @@ app.use('/app', express.static('public'))
 
 
 
-app.listen(3000);
-console.log('La app se esta ejecutando en http://localhost:3000');
+server.listen(3000, function () {
+    console.log('La app se esta ejecutando en http://localhost:3000');
+});
